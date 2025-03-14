3062303f7e9a24c64bead1732b702b9807d206a7
JonatanYMV <jonatan.medina@correounivalle.edu.co>
2025-03-14 15:45:56 -0500

    concatenacion

--- Nivel Pricipiante 1.py
+++ Nivel Pricipiante 1.py
@@ -1,3 +1,19 @@
 #Ya quedo subido el repositorio en github
 #Nivel Principiante 
 print ("Primer dia del curso")
+
+"""
+esto seria para hacer comentario de 
+mas
+de
+una linea
+"""
+
+#cadena de texto
+#pues usar "" o ''
+
+a=3
+b="texto"
+print(type(a))#tipo int
+print(type(b))#tipo str
+
---
+++ Nvel Principiante 2.py
@@ -0,0 +1,29 @@
+#Curso de la variables
+"""
+Recuerda que existen diferentes variables
+"""
+#manera de escribir con una variables
+#tener en cuenta no se debe nombra de manera diferente
+#a esto, es decir con mayuscula y minusculas
+my_string_variable="my string variable"
+print (my_string_variable)
+
+my_int_variable=2
+print(my_int_variable)
+
+my_boot_variable=False
+print(my_boot_variable)
+
+#se esta haciendp un preinterprete
+
+#nueva manera de imprimir en print
+
+#concatenacion
+print(my_boot_variable, my_int_variable,my_string_variable)
+
+#se puede cambiar el tipo de una varible si se antepone una tipo en parentesis
+#ejemplo
+my_int_to_str_variable=str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
