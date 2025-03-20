83571fb91736e2a84d89ff5b6c283840a179fa34
JonatanYMV <jonatan.medina@correounivalle.edu.co>
2025-03-20 14:19:54 -0500

    ultimo

--- Nivel Principiante 3.py
+++ Nivel Principiante 3.py
@@ -22,3 +22,10 @@ print (3>=4)
 print (3!=4)
 print("hola"=="ole")
 
+#operadores logico
+print(3>4 and "hola" > "python")
+print(3>4 or "hola" > "python")
+print(3<4 and "hola" > "python")
+print(3<4 or "hola" > "python")
+print (not(3>4 )) #lo cambia
+
