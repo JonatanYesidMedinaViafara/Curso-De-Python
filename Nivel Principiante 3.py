b6b10bdaab9f611b48b671153975a060410a1edb
JonatanYMV <jonatan.medina@correounivalle.edu.co>
2025-03-20 14:11:19 -0500

    comparaciones

---
+++ Nivel Principiante 3.py
@@ -0,0 +1,24 @@
+### Operadores ###
+print(5+5)
+print(5-5)
+print(5*5)
+print(5/5)
+
+#Operadores de modulo
+print(100%5)
+print(10%3)
+print(10//3)#con esta manera se aproxima a un entero
+print(5**3) #sirve para exponenciar
+
+print("hola "+ "me llamo "+ "jonatan " )
+
+print("esto no se puede"* 5)
+
+#operadores compartivos
+print (3>4)
+print (3<4)
+print (3<=4)
+print (3>=4)
+print (3!=4)
+print("hola"=="ole")
+
