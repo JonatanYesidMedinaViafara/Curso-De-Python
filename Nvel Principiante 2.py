2ff00840c8f52225adfc9e5eae71983197172c2f
JonatanYMV <jonatan.medina@correounivalle.edu.co>
2025-03-20 12:16:25 -0500

    clase 3

--- Nvel Principiante 2.py
+++ Nvel Principiante 2.py
@@ -27,3 +27,38 @@ my_int_to_str_variable=str(my_int_variable)
 print(my_int_to_str_variable)
 print(type(my_int_to_str_variable))
 
+#funciones del sistema
+#uso del len
+print(len(my_int_to_str_variable))
+
+print("vamos concatenar un print ",my_boot_variable  )
+
+#variables en una sola linea
+name, surame, alias ="jonatan ", "Yesid ", "Medina"
+print("me llamo: ", name , surame, "y mi alias es ", alias)
+"""
+name2=(input("what your name: "))
+lastname=(input("what your last name: "))
+
+print("your name complete is ", name2 , lastname )
+"""
+#cambiando datos de las variables
+#es facil cambiar los datos a las variables de python
+"""
+name2=(input("what your name: "))
+lastname=(input("what your last name: "))
+
+print("your name complete is ", name2 , lastname )
+"""
+#forzamos el tipo
+address: str="Yolo"
+address=5
+print(address)
+
+#coomo consultat el tipo de dato
+print(type("soy un dato str"))
+print(type(5)) #tipo int
+print(type(1.5)) #tipo float
+print(type(3+2)) #tipo complex
+print(type(True)) #tipo "boot"
+print(type(print("mi cadena de texto"))) #tipo NoneType
